113edc48360641c9f5cc6570955139a8757dbe77
Zaytri <9072119+zaytri@users.noreply.github.com>
2021-02-17 00:11:14 -0800

    Added legacy rotation animation for browsers without CSS.registerProperty
    Because OBS Browser source doesn't support it for some reason

--- AnimatedGradientFrame/widget.js
+++ AnimatedGradientFrame/widget.js
@@ -1,8 +1,23 @@
 window.addEventListener('onWidgetLoad', function (obj) {
-  CSS.registerProperty({
-    name: '--angle',
-    syntax: '<angle>',
-    initialValue: '0turn',
-    inherits: true
-  })
+  if (typeof CSS !== 'undefined' && typeof CSS.registerProperty !== 'undefined') {
+    CSS.registerProperty({
+      name: '--angle',
+      syntax: '<angle>',
+      initialValue: '0turn',
+      inherits: true
+    })
+  } else {
+    const { speed } = obj.detail.fieldData
+    const frames = speed * 60
+
+    let percentages = ''
+    for (let i = 0; i <= frames; i++) {
+      const turn = i * (1 / frames)
+      percentages = `${percentages} ${turn * 100}% { --angle: ${turn}turn; }`
+    }
+
+    const [sheet] = document.styleSheets
+    sheet.insertRule(`@keyframes gradientRotationLegacy { ${percentages} }`, sheet.length)
+    $('.gradient-rotation').css('animation-name', 'gradientRotationLegacy')
+  }
 })
