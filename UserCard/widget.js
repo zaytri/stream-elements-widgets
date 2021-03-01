eae81e959385eb074509d9bbbeca423b0a6724f8
Zaytri <9072119+zaytri@users.noreply.github.com>
2021-03-01 00:47:37 -0800

    swap animation now controlled by js
    this is to ensure that the interval is synced, and the fade is now at a consistent speed no matter the duration (was probably possible by using js to control fade in/out css classes, but i couldn't get that to work correctly)

--- UserCard/widget.js
+++ UserCard/widget.js
@@ -3,13 +3,19 @@ window.addEventListener('onWidgetLoad', obj => {
   $('.name').text(name)
   const p1 = $('.p1')
   const p2 = $('.p2')
+  const ps = $('.p1, .p2')
   const [pronouns1, pronouns2] = pronouns.split(',').map(p => p.trim())
   p1.text(pronouns1)
   p2.text(pronouns2)
   window.setInterval(_ => {
-    const p1Text = p1.text()
-  	const p2Text= p2.text()
-    p1.text(p2Text)
-    p2.text(p1Text)
+    ps.animate({ color: 'transparent' }, 500, _ => {
+      ps.animate({ color: '#fc6c85' }, 500)
+    })
+    window.setTimeout(_ => {
+      const p1Text = p1.text()
+      const p2Text= p2.text()
+      p1.text(p2Text)
+      p2.text(p1Text)
+    }, 500)
   }, fadeDuration * 1000)
 });
