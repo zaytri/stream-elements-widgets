79cfe97df62587b192a7156ae573e18069fb17be
Zaytri <9072119+zaytri@users.noreply.github.com>
2021-03-03 00:16:12 -0800

    Crossfade effect instead of fading out then in

--- UserCard/widget.js
+++ UserCard/widget.js
@@ -1,21 +1,23 @@
+let fadeState = true
+
 window.addEventListener('onWidgetLoad', obj => {
   const { fadeDuration, pronouns, name } = obj.detail.fieldData
   $('.name').text(name)
-  const p1 = $('.p1')
-  const p2 = $('.p2')
-  const ps = $('.p1, .p2')
-  const [pronouns1, pronouns2] = pronouns.split(',').map(p => p.trim())
-  p1.text(pronouns1)
-  p2.text(pronouns2)
+
+  const [p1, p2] = pronouns.split(',').map(p => p.trim())
+  $('.state1 .p1').text(p1)
+  $('.state1 .p2').text(p2)
+  $('.state2 .p2').text(p1)
+  $('.state2 .p1').text(p2)
+
   window.setInterval(_ => {
-    ps.animate({ color: 'transparent' }, 500, _ => {
-      ps.animate({ color: '#fc6c85' }, 500)
-    })
-    window.setTimeout(_ => {
-      const p1Text = p1.text()
-      const p2Text= p2.text()
-      p1.text(p2Text)
-      p2.text(p1Text)
-    }, 500)
+    if (fadeState) {
+      $('.state1 .p1, .state1 .p2').animate({ color: 'transparent' }, 'slow')
+      $('.state2 .p1, .state2 .p2').animate({ color: '#fc6c85' }, 'slow')
+    } else {
+      $('.state1 .p1, .state1 .p2').animate({ color: '#fc6c85' }, 'slow')
+      $('.state2 .p1, .state2 .p2').animate({ color: 'transparent' }, 'slow')
+    }
+    fadeState = !fadeState
   }, fadeDuration * 1000)
-});
+})
